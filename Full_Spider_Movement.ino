282aa22382028615759e0ec49d3fa9ef25e51c79
ECCPatrickSheng <84823486+ECCPatrickSheng@users.noreply.github.com>
2021-08-03 22:27:42 +1200

    Adding reset on button "5"

--- Full_Spider_Movement.ino
+++ Full_Spider_Movement.ino
@@ -123,6 +123,33 @@ void loop(){
         break ;
       case 0xFF38C7:
         Serial.println("5");
+        
+        servo1.write(180);
+        delay(1000);
+        servo2.write(180);
+        delay(1000);
+        servo3.write(180);
+        delay(1000);
+        servo4.write(180);
+        delay(1000);
+        servo5.write(180);
+        delay(1000);
+        servo6.write(180);
+        delay(1000);
+        servo7.write(180);
+        delay(1000);
+        servo8.write(180);
+        delay(1000);
+        servo1.write(90);
+        servo2.write(90);
+        servo7.write(90);
+        servo8.write(90);
+        delay(1000);
+        servo3.write(90);
+        servo4.write(90);
+        servo5.write(90);
+        servo6.write(90);
+        delay(1000);
 
         
         
